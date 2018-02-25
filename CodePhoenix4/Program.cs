547e63c451a62a341a7f081cf9d6b4839ada0b8f
Mariela Atanasova <mariela_2001at@abv.bg>
2018-02-25 13:43:27 +0200

    second

--- CodePhoenix4/Program.cs
+++ CodePhoenix4/Program.cs
@@ -10,6 +10,44 @@ namespace CodePhoenix4
     {
         static void Main(string[] args)
         {
+            string command = Console.ReadLine();
+            Dictionary<string, List<string>> squad = new Dictionary<string, List<string>>();
+       
+            while (command != "Blaze it!")
+            {
+                List<string> creatures = command.Split(' ').ToList();
+                string captain = creatures[0];
+                string mate = creatures[2];
+                if (squad.ContainsKey(captain))
+                {
+                   
+                    {
+                        squad[captain].Add(mate);
+                    }
+                }
+                else
+                {
+                    if (mate != captain)
+                    {
+                        List<string> otbor = new List<string>();
+                        otbor.Add(mate);
+                        squad.Add(captain, otbor);
+                    }
+                }
+                command = Console.ReadLine();
+               
+            }
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            foreach (var element in squad.Keys)
+            {
+                Console.WriteLine($"{element} -> {string.Join(", ", squad[element])}");
+                counts.Add(element, squad[element].Count);
+                // counts = counts.OrderByDescending(x => x.Value).ToDictionary(x => x.Key
+                Console.WriteLine(string.Join("..", counts.Keys));
+                Console.WriteLine(string.Join("..", counts.Values));
+                List<string> captains = counts.Keys.Select((x =>x )).ToList();
+                Console.WriteLine(string.Join("..", captains));
+            }
         }
     }
 }
--- phoenixgrid3/Program.cs
+++ phoenixgrid3/Program.cs
@@ -10,41 +10,73 @@ namespace phoenixgrid3
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
             List<string> commands = new List<string>();
+            string input = Console.ReadLine();
+
             while (input != "ReadMe")
             {
                 commands.Add(input);
                 input = Console.ReadLine();
 
             }
-            List<string> results = new List<string>(commands.Count);
-            for (int i = 0; i < commands.Count; i++)
+            string[] results = new string[commands.Count];
+            int index = -1; //index of rows
+            foreach (string command in commands)
             {
-                int pos = commands[i].IndexOf('.');
-                if (pos == -1 || pos!=3)
-                {
-                    results[i] = "NO";
-                    continue;
-                } //sega sum sig che imam tochka .
-                string part1 = commands[i].Substring(0,pos-1);
-                string part2 = commands[i].Substring(pos + 1);
-                
-                if (part2.Length != 3 || part1.Contains(' ') || part1.Contains('_') || part2.Contains(' ') || part2.Contains('_'))
-                {
-                    results[i] = "NO";
-                    continue;
-                }
-                char[] ch = commands[i].ToCharArray();
+                index++;
+                char[] ch = command.ToCharArray();
                 Array.Reverse(ch);
                 string reversestr = string.Join("", ch);
-                if (reversestr.Equals(commands[i]))
+                if (reversestr.Equals(command))
                 {
-                    results[i] = "YES";
+                    results[index] = "YES";
                     continue;
                 }
+                List<string> codes = command.Split('.').ToList();
+                for (int i = 0; i < codes.Count; i++)
+                {
+
+                    foreach (string code in codes)
+                    {
+                        if (codes[i].Length != 3 || codes[i].Contains(' ') || codes[i].Contains('_'))
+                        {
+                            results[index] = "NO";
+                            continue;
+                        }
+
+                    }
+                    Console.WriteLine(string.Join("\n", results));
+                }
             }
-            Console.WriteLine(string.Join("\n",results));
+
+
         }
-    }
-}
+//            for (int i = 0; i < commands.Count; i++)
+//            {
+//                int pos = commands[i].IndexOf('.');
+//                if (pos == -1 || pos!=3)
+//                {
+//                    results[i] = "NO";
+//                    continue;
+//                } //sega sum sig che imam tochka .
+//                string part1 = commands[i].Substring(0,pos-1);
+//                string part2 = commands[i].Substring(pos + 1);
+
+//                if (part2.Length != 3 || part1.Contains(' ') || part1.Contains('_') || part2.Contains(' ') || part2.Contains('_'))
+//                {
+//                    results[i] = "NO";
+//                    continue;
+//                }
+//                char[] ch = commands[i].ToCharArray();
+//                Array.Reverse(ch);
+//                string reversestr = string.Join("", ch);
+//                if (reversestr.Equals(commands[i]))
+//                {
+//                    results[i] = "YES";
+//                    continue;
+//                }
+//            }
+//            Console.WriteLine(string.Join("\n",results));
+//        }
+//    }
+//}
